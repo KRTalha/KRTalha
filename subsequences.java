5af1276caef554c519fb5fa8d6fbf8432eb61df3
KRTalha <rana.talha20003@gmail.com>
2023-12-27 16:13:13 +0100

    complete the code for subSequences

--- subsequences.java
+++ subsequences.java
@@ -1,10 +1,25 @@
 package Recursion2;
 
 public class subsequences {
+	
+	public static void subSequences(String str , int index , String newString ) {
+		if(index == str.length()){
+			System.out.println(newString);
+			return;
+		}
+		
+		
+		char currChar = str.charAt(index);
+		// to be sequences 
+		subSequences(str, index+1,newString+currChar);
+		// to be not Sequences
+		subSequences(str,index+1,newString);
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		String str = "sdf";
+		subSequences(str,0,"");
 	}
 
 }
